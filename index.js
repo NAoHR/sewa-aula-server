ab263a151c82db056f2a1c65e0d035e64f21dae6
NAoHR <najmim625@gmail.com>
2022-02-04 15:05:57 +0700

    dummy data -> real db

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const {
-    authVerifLogin,authorizeUser
+    authVerifLogin,authorizeUser,superAdminOnly
 } = require("./utils/authMiddleware/authMiddleware");
 
-const {BaseProduct} = require("./model/order.model");
+const {
+    Admin
+} = require("./model/admin.model");
 
 const app = express();
 
@@ -17,48 +19,37 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 
-app.post("/", async (req,res)=>{
-    try{
-        console.log(req.body)
-        const newData = new BaseProduct({name : req.body.name});
-        console.log(newData);
-        // await newData.save();
-        return res.json({
-            ok : true
-        })
-    }catch(e){
-        console.log(e);
-        return res.status(500).json({
-            dbStatus : false
-        })
-    }
-})
-
-app.post("/register", async (req,res) =>{
-    const {username, password} = req.body;
-    if(username !== undefined && password !== undefined){
+app.post("/addAdmin", authorizeUser , superAdminOnly ,async (req,res) =>{
+    const { username, password,role } = req.body;
+    if(username !== undefined && password !== undefined && role !== undefined){
         try{
             const newPassword = await bcrypt.hash(password,10);
-            dummyUser.push({
-                "username" : username,
-                "password" : newPassword
+            const createAdmin = new Admin({
+                username : username,
+                password : newPassword,
+                role : role,
+                dateCreated : new Date()
             })
-            console.log(dummyUser);
+            const saveIt = await createAdmin.save();
             return res.status(200).json({
                 "ok" : true
             })
-        }catch(Err){
-            console.log(Err)
+        }catch(e){
+            if(e.message.split(":")[0] === "E11000 duplicate key error collection"){
+                return res.status(500).json({
+                    ok : false,
+                    message : "username sudah terdaftar"
+                })
+            }
+            return res.status(500).json({
+                ok : false,
+                message : "server error"
+            })
         }
     }
     res.json({
         zamn : "ok"
     })
-})
-
-app.get("/testing", authorizeUser ,(req,res)=>{
-    console.log(req.detailUser);
-    res.send(req.detailUser);
 })
 
 app.post("/login", authVerifLogin,async (req,res) => {
@@ -69,24 +60,20 @@ app.post("/login", authVerifLogin,async (req,res) => {
             token : token
         })
     }catch(Err){
-        console.log(Err);
         return res.json({
             zamn : "asd"
         })
     }
 })
-// mongoose.connect(process.env.LOCAL_DEV_MONGO
-//     )
-//     .then(()=>{
-//         console.log("run")
-//         app.listen(5000, () =>{
-//             console.log("app run");
-//         })
-//     })
-//     .catch((err)=>{
-//         console.log(err);
-//     })
 
-app.listen(5000, () =>{
-    console.log("run");
-})
+mongoose.connect(process.env.LOCAL_DEV_MONGO
+    )
+    .then(()=>{
+        console.log("mongodb connected")
+        app.listen(5000, () =>{
+            console.log("express server started");
+        })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
