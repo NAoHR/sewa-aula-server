e2ebe9c3bc1c3bad752cc22c93e51129eb710332
NAoHR <najmim625@gmail.com>
2022-02-05 17:37:36 +0700

    refresh token

--- model/admin.model.js
+++ model/admin.model.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Token = require("./token.model")
+require("dotenv").config();
+
 
 const AdminScheme = mongoose.Schema({
     username : {
@@ -8,6 +12,41 @@ const AdminScheme = mongoose.Schema({
     password : String,
     role : String,
 })
+AdminScheme.methods = {
+    createAccessToken : async function () {
+        try{
+            const {_id,username} = this;
+            let AccessToken = await jwt.sign({
+                username : username,
+                id : _id
+            },process.env.SECRET_ACCESS_KEY,{
+                expiresIn : "10m"
+            });
+            return AccessToken;
+        }catch(err){
+            return false
+        }
+    },
+    createRefreshToken : async function () {
+        try{
+            const {_id,username} = this;
+            let RefreshToken = await jwt.sign({
+                username : username,
+                id : _id
+            },process.env.SECRET_REFRESH_KEY,{
+                expiresIn : "2d"
+            });
+            const saveToToken = new Token({
+                tokenId : RefreshToken
+            })
+            await saveToToken.save();
+            return RefreshToken
+        }catch(e){
+            return false;
+        }
+        
+    }
+}
 
 const Admin = mongoose.model("Admin",AdminScheme);
 
