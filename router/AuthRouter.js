eed8641f89b12ca88eaff8ca9a4ecf189baf4c05
NAoHR <najmim625@gmail.com>
2022-02-06 16:10:13 +0700

    new feature : add logout

--- router/AuthRouter.js
+++ router/AuthRouter.js
@@ -10,6 +10,8 @@ const {
     Admin
 } = require("../model/admin.model");
 
+const Token = require("../model/token.model");
+
 const router = express.Router();
 
 router.post("/login", authVerifLogin,async (req,res) => {
@@ -80,6 +82,31 @@ router.post("/refreshToken",generateRefreshToken, (req,res) => {
         );
     }catch(e){
         console.log(e)
+    }
+})
+
+router.delete("/logout",async (req,res) => {
+    try{
+        const {refreshToken} = req.body;
+        if(refreshToken){
+            let tokenDocs = await Token.findOneAndDelete({
+                tokenId : refreshToken
+            })
+            if(tokenDocs !== null){
+                return res.status(200).json({
+                    ok : true,
+                    message : "logout berhasil"
+                })
+            }
+            return res.status(400).json({
+                ok : false,
+                message : "token tidak ditemukan"
+            })
+        }
+        return res.send("please insert tokenb");
+    }catch(e){
+        console.log(e);
+        return res.send("not ok")
     }
 })
 
