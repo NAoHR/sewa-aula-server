96be88c697b0417c20cdac69afffcbe29c7e3eb1
NAoHR <najmim625@gmail.com>
2022-02-07 09:08:23 +0700

    add packet model

---
+++ model/paket.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Paket = mongoose.Schema({
+    paketPlain : {
+        type : Boolean,
+        maxLength : 300,
+        required : true
+    },
+    namaPaket : {
+        type : String,
+        maxLength : 300,
+        required : true
+    },
+    hargaAula : {
+        type : Number,
+        required : true
+    },
+    deskripsi : {
+        type : String,
+        maxLength : 300,
+        required : true
+    },
+    detailCatering : {
+        hargaPerBuah : {
+            type : Number,
+            required : function () {return this.paketPlain === false}
+        },
+        detailPaketCatering : {
+            type : [{
+                type : String,
+                required : function () {return this.paketPlain === false}
+            }],
+            required : function () {return this.paketPlain === false},
+            default : undefined
+        }
+    }
+})
+
+module.exports = mongoose.model("pakets",Paket);
